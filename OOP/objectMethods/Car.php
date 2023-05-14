c5f00bdf9147d78eb03134448741532ccfb4bce8
Анисии <117117637+anisimV@users.noreply.github.com>
2023-05-15 00:19:06 +0300

    Свойства объектов

---
+++ OOP/objectMethods/Car.php
@@ -0,0 +1,12 @@
+<?php 
+
+class Car
+{
+
+    public $color;
+    public $wheels = 4;
+    public $speed = 180;
+    var $brand;
+
+    
+}
--- index.php
+++ index.php
@@ -1,12 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-    <body>
-        
-    </body>
-</html>
+<?php 
+
+require_once 'OOP/ClassAndObject/class.php';
+require_once 'OOP/objectMethods/Car.php';
+
+function debug($data){
+    echo '<pre>' . print_r($data, return: 1) . '</pre>';
+}
+
+$car1 = new Car();
+$car2 = new Car();
+debug($car1);
+
+$car1->color = 'black';
+$car1->brand = 'bmw';
+
+$car2->color = 'cian';
+$car2->brand = 'lada';
+
+debug($car1);
+debug($car2);
+
+echo "<h3> Avto: </h3>
+Марка: {$car1->brand}<br>
+Цвет:  {$car1->color}<br>
+Колеса: {$car1->wheels}<br>
+Скорость: {$car1->speed}<br>";
